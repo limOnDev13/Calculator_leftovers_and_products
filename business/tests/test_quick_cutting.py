650da6264c7772f0714d70955f03e44197af589e
Vladimir <volosnikovvladimir69@gmail.com>
2024-04-25 16:59:46 +0500

    Rewrote testing QuickCutting algorithm

--- business/tests/test_quick_cutting.py
+++ business/tests/test_quick_cutting.py
@@ -1,23 +1,30 @@
 """
-Модуль для тестирования метода calculator.quick_cutting
+Модуль для тестирования метода QuickCutting.cut()
 """
-from business.calculator import QuickCutting
+from business.quick_cutting import QuickCutting
+from test_algorithm import TestAlgorithm
 import random
 
 
-def print_result(products: list[float], rests: list[float]):
-    quick_cutting: QuickCutting = QuickCutting(remnants=rests, products=products)
-    res_dict: dict[tuple[float, int], list[list[float]]] = quick_cutting.cut()
-    for remnant, opt_products in res_dict.items():
-        print('{}: {}'.format(remnant, opt_products))
-
+# Инициализируем тестовый класс
+test_quick_cutting: TestAlgorithm = TestAlgorithm(
+    algorithm_cls=QuickCutting,
+    min_rest_length=0.005,
+    whole_profile_length=6
+)
 
 print('Случайные значения')
 rand_products: list[float] = [round(random.uniform(0.1, 3.0), 3) for _ in range(random.randint(1, 20))]
 rand_rests: list[float] = [round(random.uniform(0.1, 4.0), 3) for _ in range(random.randint(1, 20))]
-print_result(rand_products, rand_rests)
+print('Изделия:', rand_products)
+print('Остатки:', rand_rests)
+test_quick_cutting.test_selection(rand_products, rand_rests)
 
 print('\nТест 1')
 products_test1: list[float] = [0.1, 0.1, 0.2, 0.3]
 rests_test1: list[float] = [0.15, 0.15, 0.15, 0.4, 0.05]
-print_result(products_test1, rests_test1)
+print('Изделия:', products_test1)
+print('Остатки:', rests_test1)
+test_quick_cutting.test_selection(products_test1, rests_test1)
+
+test_quick_cutting.percentage_waste(max_number_products=60, number_tests=1000, print_terminal=True)
