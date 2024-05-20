9c39d06c7a914aebacb599ceda227c1458e45ad5
Vladimir Volosnikov <volosnikovvladimir69@gmail.com>
2024-05-20 19:35:45 +0500

    Fixed checking functions. Added reactions to pressing the Calculate and Reset buttons

--- view/frames/simple_cutting_calc.py
+++ view/frames/simple_cutting_calc.py
@@ -5,9 +5,14 @@ from tkinter.ttk import Notebook, Frame, Label, Entry, Button
 from tkinter.scrolledtext import ScrolledText
 from tkinter import StringVar
 import tkinter.messagebox as msg_box
+from typing import Optional
+import re
+from re import Match
 
-from view.lexicon.lexicon_ru import LABELS, BUTTONS
-from view.view_exceptions import InputListWidthException
+from view.lexicon.lexicon_ru import LABELS, BUTTONS, ERROR_LABELS
+from view.view_exceptions import InputListWidthException, InputIntExc, InputFloatExc
+from business.quick_cutting import QuickCutting
+from business.tests.test_algorithm import TestAlgorithm
 
 
 class SimpleCutCalc:
@@ -20,24 +25,13 @@ class SimpleCutCalc:
     def __init__(self, notebook: Notebook):
         self.__notebook: Notebook = notebook
         self.__frame: Frame = Frame(notebook)
-        self.__products: StringVar = StringVar()
-        self.__remnants: StringVar = StringVar()
+        self.__input_products_text: Optional[ScrolledText] = None
+        self.__input_remnants_text: Optional[ScrolledText] = None
         self.__correction: StringVar = StringVar()
         self.__min_remnant: StringVar = StringVar()
         self.__whole_profile_len: StringVar = StringVar()
         self.__number_whole_profiles: StringVar = StringVar()
         self.__cutting_width: StringVar = StringVar()
-
-    def get_data(self, input_products: ScrolledText, input_remnants: ScrolledText,
-                 ) -> None:
-        """
-        Метод считывает введенные ширины изделий и остатков при нажатии кнопки расчет
-        :return: None
-        """
-        products_str: str = input_products.get('1.0', 'end-1c')  # По этим индексам будет считан весь текст
-        remnants_str: str = input_remnants.get('1.0', 'end-1c')
-
-        self.__products
 
     def get_frame(self) -> Frame:
         # Разделим все на разные фреймы для красоты
@@ -46,18 +40,18 @@ class SimpleCutCalc:
         input_products_label: Label = Label(input_products_and_remnants_frame, text=LABELS['input_products'])
         input_products_label.pack(anchor='nw', padx=5, pady=5)
 
-        input_products_text: ScrolledText = ScrolledText(
+        self.__input_products_text = ScrolledText(
             input_products_and_remnants_frame, width=50, height=5)
 
-        input_products_text.pack(anchor='nw', padx=5, pady=5)
+        self.__input_products_text.pack(anchor='nw', padx=5, pady=5)
 
         # 2) Ввод остатков
         input_remnants_label: Label = Label(input_products_and_remnants_frame, text=LABELS['input_remnants'])
         input_remnants_label.pack(anchor='nw', padx=5, pady=5)
 
-        input_remnants_text: ScrolledText = ScrolledText(
+        self.__input_remnants_text = ScrolledText(
             input_products_and_remnants_frame, width=50, height=5)
-        input_remnants_text.pack(anchor='nw', padx=5, pady=5)
+        self.__input_remnants_text.pack(anchor='nw', padx=5, pady=5)
         # Упакуем полученный фрейм
         input_products_and_remnants_frame.pack(anchor='nw')
 
@@ -86,17 +80,10 @@ class SimpleCutCalc:
         # 4) Добавим кнопки для расчета и и сброса введенных данных
         frame_with_buttons: Frame = Frame(self.__frame)
 
-        # 5) Добавим отслеживание переменных
-        self.__products.trace_add('read', self.__check_products)
-        self.__remnants.trace_add('read', self.__check_remnants)
-        self.__products.trace_add('read', self.__check_products)
-        self.__products.trace_add('read', self.__check_products)
-        self.__products.trace_add('read', self.__check_products)
-
         buttons: list[Button] = [
-            Button(frame_with_buttons, text=BUTTONS['quick_calc']),
+            Button(frame_with_buttons, text=BUTTONS['quick_calc'], command=self.__quick_calc_button),
             Button(frame_with_buttons, text=BUTTONS['middle_calc']),
-            Button(frame_with_buttons, text=BUTTONS['reset']),
+            Button(frame_with_buttons, text=BUTTONS['reset'], command=self.__reset_button),
         ]
         for num, button in enumerate(buttons):
             button.grid(row=0, column=num)
@@ -106,51 +93,121 @@ class SimpleCutCalc:
         return self.__frame
 
     @classmethod
-    def __check_format_list_width(cls, profiles_str: str) -> None:
+    def __check_format_list_width(cls, profiles: ScrolledText, title_error: str) -> list[float]:
         """
         Метод проверяет корректность ввода ширин нескольких изделий или остатков.
         Введенная строка должна представлять собой числа с плавающей точкой, разделенные пробелами
-        :param profiles_str: Введенная строка
-        :type profiles_str: str
-        :return: None
+        :param profiles: Объект окна, в которое вводили список ширин
+        :type profiles: ScrolledText
+        :param title_error: Название окна, в котором будет отображаться ошибка
+        :type title_error: str
+        :raise InputListWidthException: Если ввод пустой или не корректен
+        :return: Список введенных ширин
+        :rtype: list[float]
         """
+        profiles_str: str = profiles.get('1.0', 'end-1c')  # По этим индексам будет считан весь текст
         numbers_str: list[str] = profiles_str.split()
 
+        # Если ввод пустой - выбрасываем ошибку
+        if not numbers_str:
+            raise InputListWidthException(profiles_str, profiles_str, title_error)
+
         for number in numbers_str:
-            if not number.isdigit():
-                raise InputListWidthException(profiles_str, number)
+            match: Optional[Match[str]] = re.search(r'\d+\.?\d*', number)
 
-    def __check_products(self, *args) -> None:
+            # Если хотя бы одно слово не является числом - выбрасываем ошибку
+            if not match or match.end() != len(number):
+                raise InputListWidthException(profiles_str, number, title_error)
+
+        return [float(profile) for profile in numbers_str]
+
+    @classmethod
+    def __check_param(cls, param: StringVar, param_name: str) -> float:
         """
-        Метод проверяет корректность ввода списка ширин изделий
-        :param args:
+        Метод для проверки введенного параметра, который должен быть вещественным числом
+        :param param: Введенный параметр
+        :type param: StringVar
+        :param param_name: Название параметра (необходимо для красивой печати)
+        :type param_name: str
+        :raise InputFloatExc: Если введенное значение не является вещественным числом
+        :return: Введенное float значение
+        :rtype: float
+        """
+        param_str: str = param.get()
+        match: Optional[Match[str]] = re.search(r'\d+\.?\d*', param_str)
+
+        if not match or match.end() != len(param_str):
+            raise InputFloatExc(
+                title=param_name,
+                wrong_input=param_str)
+
+        return float(param_str)
+
+    def __check_number_whole_profiles(self) -> int:
+        """
+        Метод для проверки корректности введенного количества целых профилей
+        :raise InputIntExc: Если введенное значение не является вещественным числом
+        :return: Введенное количество целых профилей
+        :rtype: int
+        """
+        numer_whole_profiles: str = self.__number_whole_profiles.get()
+
+        if not numer_whole_profiles.isdigit():
+            raise InputIntExc(
+                title=ERROR_LABELS['number_whole_profiles'],
+                wrong_input=numer_whole_profiles)
+
+        return int(numer_whole_profiles)
+
+    def __quick_calc_button(self) -> None:
+        """
+        Метод срабатывает при нажатии на кнопку Быстрый расчет. Он проверяет корректность введенных значений,
+        если нет ошибок - производит расчет с помощью алгоритма QuickCutting.
+        Пока он будет выводить результат в терминал
         :return: None
         """
-        products_str = self.__products.get()
-
+        # Проверим введенные данные
         try:
-            self.__check_format_list_width(products_str)
+            products: list[float] = self.__check_format_list_width(self.__input_products_text, ERROR_LABELS['products'])
+            remnants: list[float] = self.__check_format_list_width(self.__input_remnants_text, ERROR_LABELS['remnants'])
+            corr: float = self.__check_param(self.__correction, ERROR_LABELS['correction'])
+
+            if products is not None and remnants is not None:
+                products = [product - corr
+                            for product in products]
+
+                quick_cut: QuickCutting = QuickCutting(
+                    remnants=remnants,
+                    products=products,
+                    min_rest_length=self.__check_param(self.__min_remnant, ERROR_LABELS['min_remnant']),
+                    whole_profile_length=self.__check_param(self.__whole_profile_len, ERROR_LABELS['whole_profile']),
+                    number_whole_profiles=self.__check_number_whole_profiles(),
+                    cutting_width=self.__check_param(self.__cutting_width, ERROR_LABELS['cut_width'])
+                )
+                beautiful_print: TestAlgorithm = TestAlgorithm(QuickCutting)
+                print(beautiful_print.beautiful_result(quick_cut.cut()))
+        except (InputFloatExc, InputIntExc) as exc:
+            msg_box.showerror(
+                title=ERROR_LABELS['error_input'] + exc.title,
+                message=exc.__str__()
+            )
         except InputListWidthException as exc:
             msg_box.showerror(
-                title='Ошибка ввода списка изделий',
+                title=exc.title,
                 message=exc.__str__()
             )
 
-    def __check_remnants(self, *args) -> None:
-        """
-        Метод проверяет корректность ввода списка остатков
-        :param args:
-        :return: None
-        """
-        remnants_str = self.__remnants.get()
+    def __reset_button(self) -> None:
+        if self.__input_products_text is not None:
+            self.__input_products_text.delete('1.0', 'end-1c')
+        if self.__input_remnants_text is not None:
+            self.__input_remnants_text.delete('1.0', 'end-1c')
+        self.__correction.set('')
+        self.__min_remnant.set('')
+        self.__whole_profile_len.set('')
+        self.__number_whole_profiles.set('')
+        self.__cutting_width.set('')
 
-        try:
-            self.__check_format_list_width(remnants_str)
-        except InputListWidthException as exc:
-            msg_box.showerror(
-                title='Ошибка ввода списка остатков',
-                message=exc.__str__()
-            )
 
-    def __check_digit(self, *args):
-        pass
+if __name__ == '__main__':
+    pass
