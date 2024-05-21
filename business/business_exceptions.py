76137a782232c49d92082ec12397bedce4a66481
Vladimir Volosnikov <volosnikovvladimir69@gmail.com>
2024-05-21 10:37:15 +0500

    Added an exception that will be thrown if there are not enough available residues and solid profiles for this list of products

---
+++ business/business_exceptions.py
@@ -0,0 +1,21 @@
+"""Модуль с исключениями для пакета business"""
+
+
+class NoRemnantsError(Exception):
+    """
+    Класс - исключение. Выбрасывается, когда для списка изделий не хватает остатков и цельных профилей
+    """
+    def __init__(self, title: str, current_scheme: dict[tuple[float, int], list[list[float]]]):
+        self.__title: str = title
+        self.__current_scheme: dict[tuple[float, int], list[list[float]]] = current_scheme
+
+    @property
+    def title(self) -> str:
+        return self.__title
+
+    @property
+    def current_cheme(self) -> dict[tuple[float, int], list[list[float]]]:
+        return self.__current_scheme
+
+    def __str__(self) -> str:
+        return 'Для данного списка изделий не хватает имеющихся остатков и цельных профилей!'
