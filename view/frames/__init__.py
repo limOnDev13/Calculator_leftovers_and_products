8c889c0b87c713555784635407121b400f9a666d
Vladimir Volosnikov <volosnikovvladimir69@gmail.com>
2024-05-21 18:51:03 +0500

    Fixed __init__ files

--- view/frames/__init__.py
+++ view/frames/__init__.py
@@ -1,3 +1,5 @@
 """
 Пакет отвечает за фреймы, которые объединяют различные виджеты под определенные вкладки
 """
+from .simple_cutting_calc import SimpleCutCalc
+from .result_cut_window import window_with_cut_cheme
