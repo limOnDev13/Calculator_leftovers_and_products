2bf9436f0ab7b704480a73aeea7c83ad1a6f0ee2
Vladimir Volosnikov <volosnikovvladimir69@gmail.com>
2024-05-27 18:36:46 +0500

    Added __repr__ methods

--- business/business_exceptions.py
+++ business/business_exceptions.py
@@ -5,8 +5,11 @@ class NoRemnantsError(Exception):
     """
     Класс - исключение. Выбрасывается, когда для списка изделий не хватает остатков и цельных профилей
     """
-    def __init__(self, title: str, current_scheme: dict[tuple[float, int], list[list[float]]]):
+    def __init__(self, title: str, products: list[float], remnants: list[float],
+                 current_scheme: dict[tuple[float, int], list[list[float]]]):
         self.__title: str = title
+        self.__products: list[float] = products
+        self.__remnants: list[float] = remnants
         self.__current_scheme: dict[tuple[float, int], list[list[float]]] = current_scheme
 
     @property
@@ -17,10 +20,28 @@ class NoRemnantsError(Exception):
     def current_cheme(self) -> dict[tuple[float, int], list[list[float]]]:
         return self.__current_scheme
 
+    @property
+    def products(self) -> list[float]:
+        return self.__products
+
+    @property
+    def remnants(self) -> list[float]:
+        return self.__remnants
+
     def __str__(self) -> str:
-        return 'Для данного списка изделий не хватает имеющихся остатков и цельных профилей!'
+        return 'Для данного списка изделий не хватает имеющихся остатков и цельных профилей!\n'
+
+    def __repr__(self) -> str:
+        return (f'Для данного списка изделий не хватает имеющихся остатков и цельных профилей!\n'
+                f'Список изделий: {self.products}\nСписок остатков: {self.remnants}\n'
+                f'Схема распила: {self.current_cheme}')
 
 
 class WrongSchemeError(NoRemnantsError):
     def __str__(self) -> str:
-        return f'Количество распилов больше чем количество имеющихся остатков!\nРаспил: {self.current_cheme}'
+        return 'Количество распилов больше чем количество имеющихся остатков!'
+
+    def __repr__(self) -> str:
+        return (f'Количество распилов больше чем количество имеющихся остатков!'
+                f'Список изделий: {self.products}\nСписок остатков: {self.remnants}\n'
+                f'Схема распила: {self.current_cheme}')
